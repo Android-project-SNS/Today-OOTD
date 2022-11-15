7c10a0d794502ed6e4affcd5e8142d851d60a6ee
gaLong <1000ji1@naver.com>
2022-11-16 03:23:36 +0900

    make toolbar

---
+++ app/src/main/java/com/example/today_ootd/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.today_ootd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
