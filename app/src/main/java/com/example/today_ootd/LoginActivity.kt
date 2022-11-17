01e660d66824f834c7b627fe0f010175611910ce
hahahanDW <gksehddn8@naver.com>
2022-11-18 03:18:59 +0900

    loginActivity binding

--- app/src/main/java/com/example/today_ootd/LoginActivity.kt
+++ app/src/main/java/com/example/today_ootd/LoginActivity.kt
@@ -1,35 +1,47 @@
 package com.example.today_ootd
 
+
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
-import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import com.example.today_ootd.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityLoginBinding
     var auth : FirebaseAuth? = null
 
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         auth = FirebaseAuth.getInstance()
 
         // 회원가입 버튼
-        val signup = findViewById<Button>(R.id.signupButton)
+        val signup = binding.signupButton
         signup.setOnClickListener {
             signUp()
         }
 
         // 로그인 버튼
-        val loginButton = findViewById<Button>(R.id.signInButton)
+        val loginButton = binding.signInButton
         loginButton.setOnClickListener {
             signIn()
         }
 
+        // 페이스북 로그인 버튼
+        val facebookLoginButton = binding.facebookBtn
+        facebookLoginButton.setOnClickListener {
+
+        }
     }
+
     fun signUp(){
         val signupIntent = Intent(this, SignupActivity::class.java)
         startActivity(signupIntent)
