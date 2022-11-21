53785fb1d34bb1a7a95a43debc62565a672e2c54
jaminleee <jamin_00@naver.com>
2022-11-21 20:55:55 +0900

    test data snapshot

--- app/src/main/java/com/example/today_ootd/home/HomeFragment.kt
+++ app/src/main/java/com/example/today_ootd/home/HomeFragment.kt
@@ -5,48 +5,86 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.today_ootd.R
+import com.example.today_ootd.databinding.FragmentHomeBinding
+import com.example.today_ootd.model.ArticleModel
+import com.example.today_ootd.upload.ArticleAdapter
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 
-class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class HomeFragment : Fragment(R.layout.fragment_home) {
+    private lateinit var articleDB: DatabaseReference
+    //private lateinit var userDB: DatabaseReference
+    private lateinit var articleAdapter: ArticleAdapter
+    private val articleList = mutableListOf<ArticleModel>()
+    private val listener = object: ChildEventListener {
+        override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            val articleModel = snapshot.getValue(ArticleModel::class.java)
+            articleModel ?: return
+
+            articleList.add(articleModel)
+            articleAdapter.submitList(articleList)
+
         }
+
+        override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {}
+
+        override fun onChildRemoved(snapshot: DataSnapshot) {}
+
+        override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
+
+        override fun onCancelled(error: DatabaseError) {}
+
+
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    private var binding: FragmentHomeBinding? = null
+    private val auth: FirebaseAuth by lazy {
+        Firebase.auth
     }
 
-    companion object {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val fragmentHomeBidning = FragmentHomeBinding.bind(view)
+        binding = fragmentHomeBidning
 
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        binding!!.itemRecyclerView.layoutManager = LinearLayoutManager(context)
+        binding!!.itemRecyclerView.adapter = articleAdapter
+
+        articleList.clear()
+        articleDB = Firebase.database.reference.child("OOTD")
+
+        fragmentHomeBidning.itemRecyclerView.layoutManager = LinearLayoutManager(context)
+        fragmentHomeBidning.itemRecyclerView.adapter = articleAdapter
+        articleDB.addChildEventListener(listener)
+    }
+
+//        articleAdapter.submitList(mutableListOf<ArticleModel>().apply {
+//            add(ArticleModel("1","맥북 프로16인치",100000,"1,000,000",""))
+//            add(ArticleModel("1","갤럭시S22",101010,"800,000",""))
+//        })
+
+//    }
+
+    override fun onResume() {
+        super.onResume()
+
+        articleAdapter.notifyDataSetChanged()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        articleDB.removeEventListener(listener)
     }
 }
