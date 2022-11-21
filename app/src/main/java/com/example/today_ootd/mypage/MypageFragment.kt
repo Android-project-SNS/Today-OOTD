802143f92455a1aca7f7469faddbb4e6daa1b345
hahahanDW <gksehddn8@naver.com>
2022-11-21 17:34:22 +0900

    add wheather Api

--- app/src/main/java/com/example/today_ootd/mypage/MypageFragment.kt
+++ app/src/main/java/com/example/today_ootd/mypage/MypageFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.example.today_ootd.R
+import com.example.today_ootd.databinding.FragmentMypageBinding
 import com.example.today_ootd.model.FollowModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -26,24 +28,34 @@ class MypageFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-//    private lateinit var firestore : FirebaseFirestore
-//    private lateinit var auth : FirebaseAuth
+    private var uid : String? = null
+    private var currentUid : String? = null
+    private lateinit var firestore : FirebaseFirestore
+    private lateinit var auth : FirebaseAuth
+    private var binding : FragmentMypageBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        firestore = Firebase.firestore
-//        auth = FirebaseAuth.getInstance()
 
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        firestore = Firebase.firestore
+        auth = FirebaseAuth.getInstance()
+        binding = FragmentMypageBinding.inflate(inflater, container, false)
+
+        currentUid = auth.currentUser!!.uid
+        uid = arguments?.getString("destinationUid")
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_mypage, container, false)
     }
@@ -68,29 +80,57 @@ class MypageFragment : Fragment() {
             }
     }
 
-//    // 팔로우 기능
-//    fun requestFollow(){
-//        val currentUid = auth.currentUser!!.uid
-//        var tsDocFollowing = firestore?.collection("users")?.document(currentUid!!)
-//        firestore?.runTransaction { transaction ->
-//            var followModel = transaction.get(tsDocFollowing!!).toObject(FollowModel::class.java)
-//            if (followModel == null){
-//                followModel = FollowModel()
-//                followModel!!.followingCount = 1
-//                followModel!!.followers[uid!!] = true
-//
-//                transaction.set(tsDocFollowing, followModel)
-//                return@runTransaction
-//            }
-//
-//            // 이미 팔로우한 유저라면
-//            if (followModel.following.containsKey(uid)){
-//                followModel?.followingCount = followModel?.followingCount?.minus(1)!!
-//                followModel?.followers?.remove(uid)
-//            }
-//            else{
-//                followModel?.followingCount = followModel?.followingCount?.plus(1)!!
-//            }
-//        }
-//    }
+    // 팔로우 기능
+    fun requestFollow(){
+        var tsDocFollowing = firestore?.collection("users")?.document(currentUid!!)
+        firestore?.runTransaction { transaction ->
+            var followModel = transaction.get(tsDocFollowing!!).toObject(FollowModel::class.java)
+            if (followModel == null){
+                followModel = FollowModel()
+                followModel!!.followingCount = 1
+                followModel!!.followers[uid!!] = true
+
+                transaction.set(tsDocFollowing, followModel)
+                return@runTransaction
+            }
+
+            // 이미 팔로우한 유저라면 (취소)
+            if (followModel.following.containsKey(uid)){
+                followModel?.followingCount = followModel?.followingCount?.minus(1)!!
+                followModel?.followers?.remove(uid)
+            }
+            // 팔로우 하고 있지 않다면 (팔로우)
+            else{
+                followModel?.followingCount = followModel?.followingCount?.plus(1)!!
+                followModel?.following?.set(uid!!, true)
+            }
+            transaction.set(tsDocFollowing, followModel)
+            return@runTransaction
+        }
+
+        var tsDocFollower = firestore?.collection("users")?.document(uid!!)
+        firestore?.runTransaction { transaction ->
+            var followModel = transaction.get(tsDocFollower!!).toObject(FollowModel::class.java)
+            if (followModel == null) {
+                followModel = FollowModel()
+                followModel!!.followerCount = 1
+                followModel!!.following[currentUid!!] = true
+
+                transaction.set(tsDocFollower, followModel!!)
+                return@runTransaction
+            }
+            if (followModel!!.followers.containsKey(currentUid)){
+                // 팔로우 하고 있는 경우
+                followModel!!.followerCount = followModel!!.followerCount - 1
+                followModel!!.followers.remove(currentUid)
+            }
+            else {
+                // 팔로우 하고 있지 않은 경우
+                followModel!!.followerCount = followModel!!.followerCount + 1
+                followModel!!.followers[currentUid!!] = true
+            }
+            transaction.set(tsDocFollower, followModel!!)
+            return@runTransaction
+        }
+    }
 }
