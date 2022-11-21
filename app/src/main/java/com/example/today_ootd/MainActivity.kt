a76c7ca11f9c1a89f45c8f1cdf2ac57b56f5352d
gahyeon <1000ji1@naver.com>
2022-11-21 20:20:38 +0900

    weather 작업

--- app/src/main/java/com/example/today_ootd/MainActivity.kt
+++ app/src/main/java/com/example/today_ootd/MainActivity.kt
@@ -1,15 +1,16 @@
 package com.example.today_ootd
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.today_ootd.databinding.ActivityMainBinding
 import com.example.today_ootd.favorite.FavoriteFragment
 import com.example.today_ootd.home.HomeFragment
 import com.example.today_ootd.mypage.MypageFragment
 import com.example.today_ootd.upload.UploadActivity
+import com.google.android.gms.location.LocationServices
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -52,4 +53,14 @@ class MainActivity : AppCompatActivity() {
                 commit()
             }
     }
+
+    //HomeFragment에 weather location 전달
+    private fun sendLocation(fragment: Fragment){
+        val bundle = Bundle(1) // 파라미터의 숫자는 전달하려는 값의 갯수
+
+        // 현재 위치의 날씨정보 설정하기
+        val locationClient = LocationServices.getFusedLocationProviderClient(this@MainActivity)
+        bundle.putString("location", locationClient.toString())
+    }
+
 }
