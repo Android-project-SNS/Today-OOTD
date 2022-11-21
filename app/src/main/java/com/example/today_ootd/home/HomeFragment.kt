3323c1605ce54813e4cdedeaf1681fb761477ed1
jaminleee <jamin_00@naver.com>
2022-11-22 03:35:19 +0900

    yoohoo~~~~~~~

--- app/src/main/java/com/example/today_ootd/home/HomeFragment.kt
+++ app/src/main/java/com/example/today_ootd/home/HomeFragment.kt
@@ -1,13 +1,16 @@
 package com.example.today_ootd.home
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.today_ootd.R
 import com.example.today_ootd.databinding.FragmentHomeBinding
+import com.example.today_ootd.databinding.ItemArticleBinding
 import com.example.today_ootd.model.ArticleModel
 import com.example.today_ootd.upload.ArticleAdapter
 import com.google.firebase.auth.FirebaseAuth
@@ -18,23 +21,31 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
 
 
+class HomeFragment : Fragment(R.layout.item_article) {
+    lateinit var storage: FirebaseStorage
+    private var binding: ItemArticleBinding? = null
 
-class HomeFragment : Fragment(R.layout.fragment_home) {
-//    private lateinit var articleDB: DatabaseReference
-//    //private lateinit var userDB: DatabaseReference
-//    private lateinit var articleAdapter: ArticleAdapter
-//    private val articleList = mutableListOf<ArticleModel>()
-//    private val listener = object: ChildEventListener {
-//        override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
-//
-//            val articleModel = snapshot.getValue(ArticleModel::class.java)
-//            articleModel ?: return
-//
-//            articleList.add(articleModel)
-//            articleAdapter.submitList(articleList)
-//
-//        }
-//    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val itemArticleBidning = ItemArticleBinding.bind(view)
+        binding = itemArticleBidning
+        storage = Firebase.storage
+        val storageRef = storage.reference // reference to root
+        val imageRef1 = storageRef.child("OOTD/photo/1669030395002.png")
+        displayImageRef(imageRef1, itemArticleBidning.thumbnailImageView)
+    }
+
+    private fun displayImageRef(imageRef: StorageReference?, view: ImageView) {
+        imageRef?.getBytes(Long.MAX_VALUE)?.addOnSuccessListener {
+            val bmp = BitmapFactory.decodeByteArray(it, 0, it.size)
+            view.setImageBitmap(bmp)
+        }?.addOnFailureListener {
+            // Failed to download the image
+        }
+    }
 }
